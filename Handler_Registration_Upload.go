f798d53e591a7ae9390015daefad7548d86a55ee
Alfonso Pineda <agpfven@gmail.com>
2023-08-19 22:00:53 +0200

    DB testing

--- Handler_Registration_Upload.go
+++ Handler_Registration_Upload.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"context"
+    "database/sql"
+    "log"
+    "os"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -14,4 +17,55 @@ func HandlerRegistrationUpload(phoneNumber string, isAllocatorClosed context.Con
 
 	//Compress browser sesion
 	MyZip("compress.zip", "./myUsers")
+
+}
+
+func ConnectDB() (*sql.DB ,error){
+	//Connect to database
+    db, err := sql.Open("mysql", os.Getenv("DSN"))
+    if err != nil {
+        log.Fatalf("failed to connect: %v", err)
+    }
+
+	//Ping to test connection
+    if err := db.Ping(); err != nil {
+        log.Fatalf("failed to ping: %v", err)
+    }
+
+    log.Println("Successfully connected to PlanetScale!")
+
+	return db, nil
+}
+
+func dbTest() {
+	println("j")
+	println(os.Getenv("DNS"))
+	println("j")
+	db, err := ConnectDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//Register file in Mysql system
+    filename := "compress.zip"
+
+	b, err := os.ReadFile(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+    //Creating query
+    stmt, err := db.Prepare("INSERT INTO testDB1 (pnumber, pzip) VALUES (?, ?)")
+    if err != nil{
+        log.Fatal(err)
+    }
+    pNumber := "000000000"
+
+    //Execute query
+    res, err := stmt.Exec(pNumber, b)
+    if err != nil{
+        panic(err)
+    }
+
+    println("Insert id: ", res)
 }
---
+++ statements.go
@@ -0,0 +1,9 @@
+package main
+
+const(
+	//Web pages
+	webPagesHome = "/"
+
+	//Prompts
+	PromptStartBrowser = "Initializing Browser..."
+)
