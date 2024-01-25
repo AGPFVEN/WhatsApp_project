fe2c5db493efc26d4f4fe804cc609e74cda7a29d
Alfonso Pineda <agpfven@gmail.com>
2024-01-25 22:53:01 +0100

    OAuth2 code solved

--- main.go
+++ main.go
@@ -1,14 +1,63 @@
 package main
-import "net/http"
+//import "net/http"
 
-func main(){
-	//dbTest()
+//func main(){
+	////dbTest()
 	
-	//Test: Using http.post can I send the zip file to the db?
+	////Test: Using http.post can I send the zip file to the db?
 	
-	http.HandleFunc(webPagesHome, InitialPageLoaderTest)
+	//http.HandleFunc(webPagesHome, InitialPageLoaderTest)
 	
-	//http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	////http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
-	http.ListenAndServe(":8000", nil)
+	//http.ListenAndServe(":8000", nil)
+//}
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"golang.org/x/oauth2"
+)
+
+ func main(){
+	ctx := context.Background()
+	conf := &oauth2.Config{
+		ClientID: os.Getenv("ONEDRIVE_AUTH_ID"),
+		ClientSecret: os.Getenv("ONEDRIVE_CLIENT_SECRET"),
+		Scopes: []string{"files.readwrite", "offline_access"},
+		Endpoint: oauth2.Endpoint{
+			AuthURL: os.Getenv("ONEDRIVE_AUTH_ENDPOINT"),
+			TokenURL: os.Getenv("ONEDRIVE_TOKEN_ENDPOINT"),
+		},
+	}
+	// Redirect user to consent page to ask for permission
+	// for the scopes specified above.
+	url := conf.AuthCodeURL("state", oauth2.AccessTypeOffline)
+	fmt.Printf("Visit the URL for the auth dialog: %v", url)
+
+	// Use the authorization code that is pushed to the redirect
+	// URL. Exchange will do the handshake to retrieve the
+	// initial access token. The HTTP Client returned by
+	// conf.Client will refresh the token as necessary.
+	var code string
+	if _, err := fmt.Scan(&code); err != nil {
+		log.Fatal(err)
+	}
+
+	// Use the custom HTTP client when requesting a token.
+	httpClient := &http.Client{Timeout: 2 * time.Second}
+	ctx = context.WithValue(ctx, oauth2.HTTPClient, httpClient)
+
+	tok, err := conf.Exchange(ctx, code)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client := conf.Client(ctx, tok)
+	_ = client
+	fmt.Println("It worked")
 }
--- prototyping/prototyping.go
+++ prototyping/prototyping.go
@@ -2,14 +2,16 @@ package prototyping
 
 import (
 	"context"
+	"fmt"
+	"log"
+	"net/http"
 	"os"
+	"time"
 
 	"golang.org/x/oauth2"
 )
 
-var AppConfig oauth2.Config
-
-func LoadConfig(){
+ func main(){
 	ctx := context.Background()
 	conf := &oauth2.Config{
 		ClientID: os.Getenv("ONEDRIVE_AUTH_ID"),
@@ -20,4 +22,29 @@ func LoadConfig(){
 			TokenURL: os.Getenv("ONEDRIVE_TOKEN_ENDPOINT"),
 		},
 	}
+	// Redirect user to consent page to ask for permission
+	// for the scopes specified above.
+	url := conf.AuthCodeURL("state", oauth2.AccessTypeOffline)
+	fmt.Printf("Visit the URL for the auth dialog: %v", url)
+
+	// Use the authorization code that is pushed to the redirect
+	// URL. Exchange will do the handshake to retrieve the
+	// initial access token. The HTTP Client returned by
+	// conf.Client will refresh the token as necessary.
+	var code string
+	if _, err := fmt.Scan(&code); err != nil {
+		log.Fatal(err)
+	}
+
+	// Use the custom HTTP client when requesting a token.
+	httpClient := &http.Client{Timeout: 2 * time.Second}
+	ctx = context.WithValue(ctx, oauth2.HTTPClient, httpClient)
+
+	tok, err := conf.Exchange(ctx, code)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client := conf.Client(ctx, tok)
+	_ = client
 }
