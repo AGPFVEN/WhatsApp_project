42086f8353ccfe2b6e1c3c99fbb04e983fae1a11
Alfonso Pineda <agpfven@gmail.com>
2024-01-31 19:41:52 +0100

    OneDrive API requests (figure how it works)

--- main.go
+++ main.go
@@ -1,16 +1,17 @@
 package main
+
 //import "net/http"
 
 //func main(){
-	////dbTest()
-	
-	////Test: Using http.post can I send the zip file to the db?
-	
-	//http.HandleFunc(webPagesHome, InitialPageLoaderTest)
-	
-	////http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+////dbTest()
 
-	//http.ListenAndServe(":8000", nil)
+////Test: Using http.post can I send the zip file to the db?
+
+//http.HandleFunc(webPagesHome, InitialPageLoaderTest)
+
+////http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
+//http.ListenAndServe(":8000", nil)
 //}
 import (
 	"context"
@@ -20,10 +21,12 @@ import (
 	"os"
 	"time"
 
+	"github.com/goh-chunlin/go-onedrive/onedrive"
 	"golang.org/x/oauth2"
 )
 
  func main(){
+	//Request token code----------------------------------------
 	ctx := context.Background()
 	conf := &oauth2.Config{
 		ClientID: os.Getenv("ONEDRIVE_AUTH_ID"),
@@ -34,19 +37,21 @@ import (
 			TokenURL: os.Getenv("ONEDRIVE_TOKEN_ENDPOINT"),
 		},
 	}
-	// Redirect user to consent page to ask for permission
-	// for the scopes specified above.
-	url := conf.AuthCodeURL("state", oauth2.AccessTypeOffline)
-	fmt.Printf("Visit the URL for the auth dialog: %v", url)
+	//// Redirect user to consent page to ask for permission
+	//// for the scopes specified above.
+	//url := conf.AuthCodeURL("state", oauth2.AccessTypeOffline)
+	//fmt.Printf("Visit the URL for the auth dialog: %v", url)
 
+	//Use token code-------------------------------------------
 	// Use the authorization code that is pushed to the redirect
 	// URL. Exchange will do the handshake to retrieve the
 	// initial access token. The HTTP Client returned by
 	// conf.Client will refresh the token as necessary.
-	var code string
-	if _, err := fmt.Scan(&code); err != nil {
-		log.Fatal(err)
-	}
+	print("hi")
+	code := os.Getenv("ONEDRIVE_TOKEN_REDEEM_CODE")
+	//if _, err := fmt.Scan(&code); err != nil {
+		//log.Fatal(err)
+	//}
 
 	// Use the custom HTTP client when requesting a token.
 	httpClient := &http.Client{Timeout: 2 * time.Second}
@@ -58,6 +63,14 @@ import (
 	}
 
 	client := conf.Client(ctx, tok)
-	_ = client
 	fmt.Println("It worked")
+
+	//----------------------------OneDrive Mine------------------
+	lib_client := onedrive.NewClient(client)
+	lib_ctx := context.Background()
+	somenoi, err := lib_client.DriveItems.List(lib_ctx, "")
+	if err != nil{
+		log.Fatal(err)
+	}
+	print(somenoi.DriveItems)
 }
