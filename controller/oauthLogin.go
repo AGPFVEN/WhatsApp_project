cc0e2d155fd904308001c8ca0fe97a2f45c94baa
Alfonso Pineda <agpfven@gmail.com>
2024-02-14 00:50:19 +0100

    callback, not even debugged

--- controller/oauthLogin.go
+++ controller/oauthLogin.go
@@ -1,6 +1,8 @@
 package controller
 
 import (
+	"context"
+	"fmt"
 	"net/http"
 
 	"github.com/agpfven/WhatsApp_project/config"
@@ -12,4 +14,27 @@ func OnedriveLogin(w http.ResponseWriter, r *http.Request){
 	url := onedriveConfig.AuthCodeURL("randomstate")
 
 	http.Redirect(w, r, url, http.StatusSeeOther)
+}
+
+func OnedriveCallback(res http.ResponseWriter, req *http.Request){
+	//State
+	state := req.URL.Query()["state"][0]
+	if state != "randomstate" {
+		fmt.Fprintln(res, "States doesn't match")
+		return
+	}
+
+	//Code
+	code := req.URL.Query()["code"][0]
+
+	//Configuration
+	onedriveConfig := config.LoadOauthConfig()
+
+	//Exchange code for token
+	token, err := onedriveConfig.Exchange(context.Background(), code)
+	if err != nil {
+		fmt.Fprintln(res, "Code-Token exchange failed")
+	}
+
+	//use token????
 }
